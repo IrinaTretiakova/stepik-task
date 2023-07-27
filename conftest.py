4e1883b51f32fc2278d9fbc8ea5824458cf9929f
Irina Tretiakova <iri54696376@gmail.com>
2023-07-27 22:07:29 +0200

    Configuration file with command line handler and 'Options'

---
+++ conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
+    print("\nstart browser for test..")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
